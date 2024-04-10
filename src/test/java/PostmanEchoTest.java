a9adf0227a7dc1b1b1c698a5f42564b6b987a146
mega test <mahp0@mail.ru>
2024-04-10 20:47:31 +0300

    Error fixed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("just a couple of words to make sure the test fail"));
+                .body("data", equalTo("just a couple of words"));
         ;
     }
 }
